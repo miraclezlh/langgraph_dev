174dc86c6bc426808dfaf18790e04e8a0e51da83
season <706350462@qq.com>
2026-08-29 00:50:49 +0800

    LangGraph 展示图像

--- src/agent/control/LangGraphBranch.py
+++ src/agent/control/LangGraphBranch.py
@@ -7,6 +7,9 @@ from IPython.display import Image,display
 from langchain_core.messages import AnyMessage, AIMessage, HumanMessage
 from langgraph.graph import StateGraph, MessagesState
 from langgraph.constants import START, END
+from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
+
 
 # 定义节点间交互的状态类
 # Annotated允许为类型，提供额外的元数据；而不影响类型检查
@@ -46,15 +49,19 @@ builder.add_edge("d",END)
 graph = builder.compile()
 
 # Mermaid是基于文本的图表，和流程图的可视化工具
-display(Image(graph.get_graph().draw_mermaid_png()))
+try:
+    # 使用 Mermaid 生成图表并保存为文件
+    mermaid_code = graph.get_graph().draw_mermaid_png()
+    with open("graph.jpg", "wb") as f:
+        f.write(mermaid_code)
 
+    # 使用 matplotlib 显示图像
+    img = mpimg.imread("graph.jpg")
+    plt.imshow(img)
+    plt.axis('off')  # 关闭坐标轴
+    plt.show()
+except Exception as e:
+    print(f"An error occurred: {e}")
 
-# app = FastAPI()
-# # 注册调试服务
-# app.include_router(get_langgraph_server(graph), prefix="/langgraph")
-#
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8123)
 
 
