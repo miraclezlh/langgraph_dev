aa07ee5d4f5bebd1f7e1583b92587c467ca5c7a3
season <706350462@qq.com>
2026-09-07 20:37:46 +0800

    LangGraph 条件分支例子2

--- src/agent/control/LangGraphLoopBranch.py
+++ src/agent/control/LangGraphLoopBranch.py
@@ -6,7 +6,7 @@ from langgraph.constants import START, END
 from langgraph.graph import StateGraph
 from matplotlib import pyplot as plt
 
-# 条件分支的例子
+# 节点分支的例子
 
 # 定义节点间交互的状态类
 # Annotated允许为类型，提供额外的元数据；而不影响类型检查
@@ -23,45 +23,53 @@ def b(state: State):
     print(f"添加'B'到{state['aggregate']}")
     return {"aggregate": ["B"]}
 
+
+def c(state: State):
+    print(f"添加'C'到{state['aggregate']}")
+    return {"aggregate": ["C"]}
+
+
+def d(state: State):
+    print(f"添加'D'到{state['aggregate']}")
+    return {"aggregate": ["D"]}
+
 def route(state: State) -> Literal["b",END]:
     if len(state["aggregate"]) <7 :
         return "b"
     else:
         return END
 
+
 builder = StateGraph(State)
 builder.add_node(a)
 builder.add_node(b)
+builder.add_node(c)
+builder.add_node(d)
 
-
+# 添加edge边
 builder.add_edge(START, "a")
-builder.add_conditional_edges("a",route)
-builder.add_edge("b", "a")
+builder.add_conditional_edges("a",route) # 条件边
+builder.add_edge("b", "c")
+builder.add_edge("b", "d")
+builder.add_edge(["c","d"], "a")
+
+# 编译
 graph = builder.compile()
 
 # Mermaid是基于文本的图表，和流程图的可视化工具
 try:
     # 使用 Mermaid 生成图表并保存为文件
     mermaid_code = graph.get_graph().draw_mermaid_png()
-    with open("conditional_graph.jpg", "wb") as f:
+    with open("branch_loop_graph.jpg", "wb") as f:
         f.write(mermaid_code)
 
     # 使用 matplotlib 显示图像
-    img = mpimg.imread("conditional_graph.jpg")
+    img = mpimg.imread("branch_loop_graph.jpg")
     plt.imshow(img)
     plt.axis('off')  # 关闭坐标轴
     plt.show()
 except Exception as e:
     print(f"An error occurred: {e}")
 
-# recursion_limit 限制循环;configurable 配置线程id等
-try:
-    result = graph.invoke({"aggregate": []}, {
-        "configurable": {"thread_id": "test"},
-        "recursion_limit":3
-
-    })
-    print(result)
-except RecursionError as e:
-    print(f"Recursion Error: {e}")
-
+result = graph.invoke({"aggregate": []}, {"configurable": {"thread_id": "test"}})
+print(result)
